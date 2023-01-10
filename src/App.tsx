d3c3cf00d22bc73d8612d1e1a5649ebe43002bb6
leoarthurwall <leowall93@gmail.com>
2023-01-11 00:16:54 +0100

    created the styled ocmponents for the table - need to style now

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import Users from "./Components/Users/Users";
 import styled from "styled-components";
 
 const Container = styled.main`
-height: 100%;
-width: 100%;
-display: flex;
-justify-content: center;
-flex-direction: column;
-align-items: center;
-`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   return (
--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -2,11 +2,28 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.section`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: row;
+`;
+const Name = styled.div`
 `
+const NameData = styled.p``
+const Email = styled.div``
+const EmailData = styled.p``
+const Phone = styled.div``
+const PhoneData = styled.p``
+const Photo = styled.div``
+const PhotoData = styled.div``
+const Show = styled.div``
+const ShowData = styled.button``
+const Heading = styled.h4``
 
 const Users = () => {
+  const [userData, setUserData] = useState([]); //STORES USER DATA FROM API
 
-  const [userData, setUserData] = useState([]);
+  //API FETCH FOR USER DATA
   const API = "https://randomuser.me/api/?results=20";
   const fetchUserData = () => {
     fetch(API)
@@ -21,7 +38,28 @@ const Users = () => {
   }, []);
 
   console.log({ userData });
-  return <Container>Users</Container>;
+  return <Container>
+   <Name>
+    <Heading></Heading>
+    <NameData></NameData>
+   </Name>
+   <Email>
+    <Heading></Heading>
+    <EmailData></EmailData>
+   </Email>
+   <Phone>
+    <Heading></Heading>
+    <PhoneData></PhoneData>
+   </Phone>
+   <Photo>
+    <Heading></Heading>
+    <PhotoData></PhotoData>
+   </Photo>
+   <Show>
+    <Heading></Heading>
+    <ShowData></ShowData>
+   </Show>
+  </Container>;
 };
 
 export default Users;
